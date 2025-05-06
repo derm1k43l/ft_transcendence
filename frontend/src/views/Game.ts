cf0e67667bb67d10f59df14b04a1b44596661912
Cosmin - Nicolae Stoia <cstoia@2-C-4.42heilbronn.de>
2025-05-06 10:52:48 +0200

    + | Added comments in Game.ts file and game.css

--- frontend/src/views/Game.ts
+++ frontend/src/views/Game.ts
@@ -1,14 +1,17 @@
 import { Router } from "../core/router";
 import { PongGame } from "../game/PongGame.js";
 
+// The GameView class is responsible for rendering the game view and handling user input to start the game
 export class GameView {
+    // Game instance (initially null) and Router instance, passed through constructor
     private game: PongGame | null = null;
     private router: Router;
 
     constructor(router: Router) {
         this.router = router;
     }
-
+    
+    // The render method creates the HTML structure for the game mode selection and renders it to the provided container
     render(container: HTMLElement) {
         container.innerHTML = `
             <div id="modeContainer">
@@ -19,12 +22,14 @@ export class GameView {
             <div id="game-container"></div>
         `;
 
+        // Querying DOM elements for buttons and game container
         const modeContainer = container.querySelector('#modeContainer') as HTMLElement;
         const singleplayerButton = container.querySelector('#singleplayerButton') as HTMLButtonElement;
         const multiplayerKeyboardButton = container.querySelector('#multiplayerKeyboardButton') as HTMLButtonElement;
         const multiplayerRemoteButton = container.querySelector('#multiplayerRemoteButton') as HTMLButtonElement;
         const gameContainer = container.querySelector('#game-container') as HTMLElement;
 
+        // Event listener for single-player and multiplayer mode button
         singleplayerButton.addEventListener('click', () => {
             modeContainer.style.display = 'none';
             this.startGame(gameContainer, true);
@@ -36,11 +41,13 @@ export class GameView {
         });
     }
 
+    // Starts the Pong game, passing the container and the mode (single player or multiplayer)
     private startGame(container: HTMLElement, isSinglePlayer: boolean) {
         this.game = new PongGame(container);
         this.game.start(isSinglePlayer);
     }
 
+    // Destroys the game instance if its not null
     destroy() {
         this.game?.destroy();
         this.game = null;
